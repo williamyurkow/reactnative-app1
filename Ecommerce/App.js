ef9d1121f387d4e170e28a2ba35ef710ba170b14
William Yurkow <williamy@tlcengine.com>
2021-01-11 12:57:11 -0500

    created Header, importing flatlist now attempting to display images onto the page for the seller list.

--- Ecommerce/App.js
+++ Ecommerce/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
 import Header from './components/Header';
+import {uuid} from 'uuidv4';
 
 const App = () => {
+  const [items, setItems] = useState([
+    {id: uuid(), text: 'Accessories'},
+    {id: uuid(), text: 'Top'},
+    {id: uuid(), text: 'Bottom'},
+    {id: uuid(), text: 'Footwear'},
+  ]);
+
   return (
     <View style={styles.container}>
       <Header />
+      <FlatList
+        data={items}
+        renderItem={({item}) => <Text>{item.text}</Text>}
+      />
     </View>
   );
 };
@@ -13,7 +25,11 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 75,
   },
 });
 
 export default App;
+
+//Video 25:14 having trouble getting the header to display the page. import trouble with the top import on both pages.
+// look into why it isnt displaying or importing.
--- Ecommerce/components/Header.js
+++ Ecommerce/components/Header.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
-const Header = () => {
+const Header = ({title}) => {
   return (
     <View style={styles.header}>
-      <Text style={styles.text}>Hello World</Text>
+      <Text style={styles.text}>{title}</Text>
     </View>
   );
+};
+
+Header.defaultProps = {
+  title: 'Clothing For Sale',
 };
 
 const styles = StyleSheet.create({
   header: {
     height: 60,
     padding: 15,
-    backgroundColor: 'gainsboro',
+    backgroundColor: '#000000',
   },
   text: {
     color: '#fff',
